a96e4b5980c6376a1cb5174b04ff8d03ef2d3664
Tayssa Freire <tayssafreiremesquita@gmail.com>
2026-08-18 10:11:33 -0300

    Guards Interceptors

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { authGuard } from './core/auth.guard';
+import { adminGuard } from './core/admin.guard';
 import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
@@ -23,6 +24,11 @@ export const routes: Routes = [
   {
     path: 'login',
     loadComponent: () => import('./features/login/login/login').then((m) => m.Login),
+  },
+  {
+    path: 'admin',
+    canActivate: [adminGuard],
+    loadComponent: () => import('./features/admin/admin/admin').then((m) => m.Admin),
   },
   {
     path: '**',
